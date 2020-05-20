df9f91237189961d38b59668d980550eb71f5ebe
VaerenTokoyami <srmchugh85@gmail.com>
2020-05-20 03:56:01 -0400

    Streamlined source downloads, added descriptions, and verified date/time loads were correct

--- plot1.R
+++ plot1.R
@@ -1,28 +1,53 @@
 # This is my first plot for the JHU Exploratory Data Analysis Week 1 Course Project
 # Good Luck to everyone- Best Regards, SRM
 
+
+
 # Step 1 - Identify Data Repository Source Origin Data
+
+## 1.1 Create Variable to store the dataset URL from project prompt
 SourceURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+## 1.2 Create variable to store the dataset filename from the URL from the project prompt
 SourceFileName <- "household_power_consumption.zip"
+
 
 
 # Step 2 - Download, Unzip, & Load the Source Origin Data File
 
-## Step 2.1 - Download
-
+## Step 2.1 - Download (via an if argument so we're not re-downloading the source file for every plot)
 if(!file.exists(SourceFileName)) {
   download.file(SourceURL,SourceFileName, mode="wb")
 }
 
-## Step 2.2 - Unzip
-
-SourceDest <- "household_power_consumption"
+## Step 2.2 - Unzip (via an if argument so we're not re-creating the local source file for every plot)
+SourceDest <- "household_power_consumption.txt"
 if(!file.exists(SourceDest)) {
   unzip(SourceFileName)
 }
 
-# Step 2.3 - Load the Data
+## Step 2.3 - Load the Data
+powerData <- read.table(SourceDest, header=TRUE, sep=";")
 
-powerConsumptionDataFile <- paste(SourceDest,".txt", sep = "")
-powerData <- read.table(powerConsumptionDataFile)
-head(powerData)
+
+
+
+# Step 3 - Correct Date Formats and Subset Data as Required by Project Prompt
+
+## 3.1 Format the dates within the 'Date' variable of 'powerData' into a R-readable date format
+powerData$Date <- as.Date(powerData$Date, format="%d/%m/%Y")
+
+## 3.2 Create the next subset of the imported data that we'll be utilizing to create the plots
+febData <- subset(powerData, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+
+
+# Step 4 - Convert Dates/Times into POSIXct compliant datetime values
+
+## 4.1 Create temp variable 'datetime'
+datetime <- paste(as.Date(febData$Date), febData$Time)
+
+## 4.2 Create a new variable as part of febData table known as 'datetime'
+febData$datetime <- as.POSIXct(datetime)
+
+# Step 5 - 
